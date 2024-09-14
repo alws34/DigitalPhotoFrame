acceac4ddb83a6625845678e7acfce04622f2782
alws34 <alws34@gmail.com>
2024-09-15 00:22:46 +0300

    started implementing api

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from PhotoFrame import PhotoFrame
+from flask import Flask
+from flask_cors import CORS
+from flask_restful import Resource, Api
 
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
 
 if __name__ == "__main__":
     frame = PhotoFrame()
